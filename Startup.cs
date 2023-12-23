7841e7ad8d785dd2568b3878ede33581c1bce968
JosephHartman76 <150567584+JosephHartman76@users.noreply.github.com>
2023-12-22 20:45:33 -0700

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -11,7 +11,6 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;  // Add this using directive
 using StudentExams.DbContext;
 
-
 namespace StudentExams
 {
     public class Startup
@@ -23,16 +22,14 @@ namespace StudentExams
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-
         {
             services.AddDbContext<SchoolDbContext>(options =>
                 options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -42,7 +39,6 @@ namespace StudentExams
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -56,6 +52,11 @@ namespace StudentExams
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+
+                // Add your custom route here
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
