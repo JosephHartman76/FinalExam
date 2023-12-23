f05bfda9d2a120d2bc7e5d8c04634806b326eb1e
JosephHartman76 <150567584+JosephHartman76@users.noreply.github.com>
2023-12-22 18:19:02 -0700

    Create Index.cshtml.cs

---
+++ Views/Teacher/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudentExams.DbContext;
+using StudentExams.Models;
+
+namespace StudentExams.Pages.Teacher
+{
+    public class IndexModel : PageModel
+    {
+        private readonly SchoolDbContext _dbContext;
+
+        public IndexModel(SchoolDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Teacher> Teachers { get; set; }
+
+        public void OnGet()
+        {
+            Teachers = _dbContext.Teachers.ToList();
+        }
+    }
+}
